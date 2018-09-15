aea99ee14b69566286a97b21ce5d91fb7b24ccbd
Jeff Cairns <jeffca@Jeffs-MacBook-Pro.local>
2018-09-14 18:53:08 -0700

    first commit

--- app.js
+++ app.js
@@ -213,6 +213,6 @@ app.get("*", function(req, res) {
    res.send("This shows on all pages except the routes above this one"); 
 });
 
-app.listen(3000, process.env.IP, function() {
+app.listen(process.env.PORT || 5000, process.env.IP, function() {
    console.log("Server has started");
 });
