1986c7ad271393d56a4f38e154e186d973548568
Jeff Cairns <jeffca@Jeffs-MacBook-Pro.local>
2018-09-15 20:13:32 -0700

    third cocmmit

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ methodOverride = require("method-override");
 // mongoose.connect("mongodb://localhost/runtracking", {useNewUrlParser: true});
 
 const Sequelize = require("sequelize");
+
+console.log(" I AM HERE");
+
 const sequelize = new Sequelize('d44r132kdeefer', 'hvwvyrbinytxga', '80ddf8ed99a35b2e20971aeb0c5b41bd8fe16009b57fe17083569a74b45aad55', {
 	dialect: "postgres",
 	host: "ec2-107-21-126-193.compute-1.amazonaws.com",
@@ -19,14 +22,16 @@ const sequelize = new Sequelize('d44r132kdeefer', 'hvwvyrbinytxga', '80ddf8ed99a
 	native: true
 });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+console.log("I GOT HERE TOO");
+
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
 
 const Trail = sequelize.define("trail", {
 	title: Sequelize.STRING,
